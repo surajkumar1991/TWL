566c7fc2c7a9aad37a54f2080c8352a8abbac59d
Akhilesh palalndurkar <akhilesh.palandurkar@vuclip.com>
2019-09-28 14:18:18 +0530

    commit Clean up

--- Test Suites/Data driven tests.ts
+++ Test Suites/Data driven tests.ts
@@ -11,51 +11,6 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>0c071e44-c30c-46a8-9063-e68816294632</testSuiteGuid>
    <testCaseLink>
-      <guid>23aa5fea-5d59-4697-a4ea-600ff8eb7bd3</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Web API Tests/Advance Tests/EXECUTABLE/api-2-issue-tests/Scenario - Create ticket successfully with API</testCaseId>
-      <testDataLink>
-         <combinationType>ONE</combinationType>
-         <id>4c501795-805e-4f97-b4eb-dff78b6167d7</id>
-         <iterationEntity>
-            <iterationType>ALL</iterationType>
-            <value></value>
-         </iterationEntity>
-         <testDataId>Data Files/Issue information</testDataId>
-      </testDataLink>
-      <variableLink>
-         <testDataLinkId>4c501795-805e-4f97-b4eb-dff78b6167d7</testDataLinkId>
-         <type>DATA_COLUMN</type>
-         <value>project_key</value>
-         <variableId>1b1d3731-082d-4581-96f9-f8096f7053aa</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId>4c501795-805e-4f97-b4eb-dff78b6167d7</testDataLinkId>
-         <type>DATA_COLUMN</type>
-         <value>issue_type</value>
-         <variableId>a9377ed8-29a0-4962-a3c5-7fb6a437265f</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId>4c501795-805e-4f97-b4eb-dff78b6167d7</testDataLinkId>
-         <type>DATA_COLUMN</type>
-         <value>priority</value>
-         <variableId>035beca4-79a5-4a4e-a7d0-9a15eb2216d5</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId>4c501795-805e-4f97-b4eb-dff78b6167d7</testDataLinkId>
-         <type>DATA_COLUMN</type>
-         <value>summary</value>
-         <variableId>61abdf8c-30b0-410d-92d6-6c715fad9afd</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId>4c501795-805e-4f97-b4eb-dff78b6167d7</testDataLinkId>
-         <type>DATA_COLUMN</type>
-         <value>description</value>
-         <variableId>2619d56d-67a6-4d53-8b49-36e5e6e72947</variableId>
-      </variableLink>
-   </testCaseLink>
-   <testCaseLink>
       <guid>cccf5cef-f91a-49b6-a033-832aa5ec9788</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
